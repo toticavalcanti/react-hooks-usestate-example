5ebc8f323b0d896839e4b2996a8ad40551f9aa3e
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-01-23 00:44:40 -0300

    class

--- src/components/use-state-component/use-state-example-component.jsx
+++ src/components/use-state-component/use-state-example-component.jsx
@@ -2,17 +2,6 @@ import React from 'react';
 
 import Card from '../card/card.component';
 
-// const UseStateExample = () => {
-//   const [name, setName] = useState('Toti');
-
-//   return (
-//     <Card>
-//       <h1> {name} </h1>
-//       <button onClick={(e) => setName(name === 'Fulano' ? 'Beltrano' : 'Fulano')}>Mudar</button>
-//     </Card>
-//   );
-// };
-
 export class StateClassComponent extends React.Component {
   constructor() {
     super();
@@ -24,19 +13,39 @@ export class StateClassComponent extends React.Component {
   }
 
   componentDidMount(){
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchValue !== this.state.searchQuery) {
+      this.fetchData(this.state.searchValue);
+   }
+    this.fetchData()
+  }
+    
+  fetchData() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(resp => resp.json())
-      .then(data => this.setState({ user: data}))
-    }
+    .then(resp => resp.json())
+    .then(resJson => this.setState({ user: resJson[0]}))
+  }
+
+  /* handleChange() function to set a new state for input */
+  handleInputChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      searchQuery: event.target.value
+    })
+  }
 
   render() {
     return (
       <Card> 
-        <input type='search' 
-        value={this.state.searchQuery} 
-        onChange={event => this.state.searchQuery(event.target.value)} 
-      /> 
-      {this.state.user ? ( 
+        <input 
+          type='search' 
+          value={this.state.searchQuery} 
+          onChange={(e) => this.handleInputChange(e)} 
+        /> 
+        {this.state.user ? ( 
         <div> 
           <h3>{this.state.user.name}</h3> 
           <h3>{this.state.user.username}</h3> 
